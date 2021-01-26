50fe25ec39f28f0b01f53dc1d491af0bd2a6f05f
Ciaran O'Brien <ciaranrobrien@gmail.com>
2021-01-26 20:02:41 +0000

    Fixed macOS compatibility.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftUIDelayedGesture",
     platforms: [
-        .iOS(.v14), .watchOS(.v7), .macOS(.v10_15)
+        .iOS(.v14), .watchOS(.v7), .macOS(.v11)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
