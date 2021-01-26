2361413b2271fe0812eda703bc95c8dfbe2b5d7f
Ciaran O'Brien <ciaranrobrien@gmail.com>
2021-01-26 20:25:31 +0000

    Removed Resources

--- Package.swift
+++ Package.swift
@@ -23,7 +23,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftUIDelayedGesture",
-            dependencies: []),
+            dependencies: [],
+            exclude: ["Resources"]),
         .testTarget(
             name: "SwiftUIDelayedGestureTests",
             dependencies: ["SwiftUIDelayedGesture"]),
